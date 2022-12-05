d09ffd06ddc69a5638b014c412e9ee3a110d861b
BrunoSoaresV <brunoverissimo398@gmail.com>
2022-12-05 18:18:25 -0300

    Modificação

--- editar.php
+++ editar.php
@@ -26,24 +26,24 @@ while ($linha = pg_fetch_assoc($resultado)) {
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <link href="assets/img/imagem1.png" rel="icon">
-  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+  <link href="static/imagem1.png" rel="icon">
+  <link href="static/apple-touch-icon.png" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
   <link href="https://fonts.gstatic.com" rel="preconnect">
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="assets/vendor/quill/quill.snow.css" rel="stylesheet">
-  <link href="assets/vendor/quill/quill.bubble.css" rel="stylesheet">
-  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-  <link href="assets/vendor/simple-datatables/style.css" rel="stylesheet">
+  <link href="static/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="static/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="static/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="static/quill/quill.snow.css" rel="stylesheet">
+  <link href="static/quill/quill.bubble.css" rel="stylesheet">
+  <link href="static/remixicon/remixicon.css" rel="stylesheet">
+  <link href="static/simple-datatables/style.css" rel="stylesheet">
 
   <!-- Template Main CSS File -->
-  <link href="assets/css/style1.css" rel="stylesheet">
+  <link href="static/style1.css" rel="stylesheet">
   <!-- =======================================================
   * Template Name: NiceAdmin - v2.2.2
   * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
@@ -164,16 +164,16 @@ while ($linha = pg_fetch_assoc($resultado)) {
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
   <!-- Vendor JS Files -->
-  <script src="assets/vendor/apexcharts/apexcharts.min.js"></script>
-  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="assets/vendor/chart.js/chart.min.js"></script>
-  <script src="assets/vendor/echarts/echarts.min.js"></script>
-  <script src="assets/vendor/quill/quill.min.js"></script>
-  <script src="assets/vendor/simple-datatables/simple-datatables.js"></script>
-  <script src="assets/vendor/tinymce/tinymce.min.js"></script>
-  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="static/apexcharts/apexcharts.min.js"></script>
+  <script src="static/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="static/chart.js/chart.min.js"></script>
+  <script src="static/echarts/echarts.min.js"></script>
+  <script src="static/quill/quill.min.js"></script>
+  <script src="static/simple-datatables/simple-datatables.js"></script>
+  <script src="static/tinymce/tinymce.min.js"></script>
+  <script src="static/php-email-form/validate.js"></script>
   <!-- Template Main JS File -->
-  <script src="assets/js/main1.js"></script>
+  <script src="static/main1.js"></script>
 </body>
 
 </html>
