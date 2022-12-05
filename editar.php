03e61062d0157692e8f48fdd07eb41aab6d383e3
BrunoSoaresV <brunoverissimo398@gmail.com>
2022-12-05 18:17:04 -0300

    Modificação

--- editar.php
+++ editar.php
@@ -11,9 +11,8 @@ while ($linha = pg_fetch_assoc($resultado)) {
   $nome = $linha['nome'];
   $email = $linha['email'];
   $email_empresa = $linha['email_empresa'];
-  $ctps1 = $linha['ctps'];
+  $ctps = $linha['ctps'];
 }
-$ctps = str_pad($ctps1, 16, 0, STR_PAD_LEFT);
 ?>
 
 <!DOCTYPE html>
@@ -27,24 +26,24 @@ $ctps = str_pad($ctps1, 16, 0, STR_PAD_LEFT);
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <link href="static/imagem1.png" rel="icon">
-  <link href="static/apple-touch-icon.png" rel="apple-touch-icon">
+  <link href="assets/img/imagem1.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
   <link href="https://fonts.gstatic.com" rel="preconnect">
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="static/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="static/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="static/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="static/quill/quill.snow.css" rel="stylesheet">
-  <link href="static/quill/quill.bubble.css" rel="stylesheet">
-  <link href="static/remixicon/remixicon.css" rel="stylesheet">
-  <link href="static/simple-datatables/style.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/quill/quill.snow.css" rel="stylesheet">
+  <link href="assets/vendor/quill/quill.bubble.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/simple-datatables/style.css" rel="stylesheet">
 
   <!-- Template Main CSS File -->
-  <link href="static/style1.css" rel="stylesheet">
+  <link href="assets/css/style1.css" rel="stylesheet">
   <!-- =======================================================
   * Template Name: NiceAdmin - v2.2.2
   * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
@@ -116,7 +115,7 @@ $ctps = str_pad($ctps1, 16, 0, STR_PAD_LEFT);
                     <label for="s">Status:</label><br>
 
                     <?php
-                    $sql = "SELECT * FROM refape_web.funcionario WHERE email_empresa='$email_empresa' and ctps='$ctps' ;";
+                    $sql = "SELECT * FROM refape_web.funcionario WHERE id='$id';";
                     $resultado=pg_query($conexao, $sql);
                     $linha = pg_fetch_assoc($resultado);
                     if($linha){
@@ -125,11 +124,11 @@ $ctps = str_pad($ctps1, 16, 0, STR_PAD_LEFT);
                           $status = $linha['status'];
                       }
                       if ($status=='f'){
-                      echo '<input type="radio" name="s" placeholder="Status" value="true" id="s" checked required><label for="true">&nbsp;Ativado</label><br>
+                      echo '<input type="radio" name="s" placeholder="Status" value="true" id="s" required><label for="true">&nbsp;Ativado</label><br>
                       <input type="radio" name="s" placeholder="Status" value="false"  id="s" required><label for="false">&nbsp;Desativado</label>';
                     }else{
                       echo '<input type="radio" name="s" placeholder="Status" value="true" id="s"  required><label for="true">&nbsp;Ativado</label><br>
-                      <input type="radio" name="s" placeholder="Status" value="false"  id="s" checked required><label for="false">&nbsp;Desativado</label>';
+                      <input type="radio" name="s" placeholder="Status" value="false"  id="s"  required><label for="false">&nbsp;Desativado</label>';
                     }
                   }
                      ?>
@@ -165,16 +164,16 @@ $ctps = str_pad($ctps1, 16, 0, STR_PAD_LEFT);
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
   <!-- Vendor JS Files -->
-  <script src="static/apexcharts/apexcharts.min.js"></script>
-  <script src="static/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="static/chart.js/chart.min.js"></script>
-  <script src="static/echarts/echarts.min.js"></script>
-  <script src="static/quill/quill.min.js"></script>
-  <script src="static/simple-datatables/simple-datatables.js"></script>
-  <script src="static/tinymce/tinymce.min.js"></script>
-  <script src="static/php-email-form/validate.js"></script>
+  <script src="assets/vendor/apexcharts/apexcharts.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/chart.js/chart.min.js"></script>
+  <script src="assets/vendor/echarts/echarts.min.js"></script>
+  <script src="assets/vendor/quill/quill.min.js"></script>
+  <script src="assets/vendor/simple-datatables/simple-datatables.js"></script>
+  <script src="assets/vendor/tinymce/tinymce.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
   <!-- Template Main JS File -->
-  <script src="static/main1.js"></script>
+  <script src="assets/js/main1.js"></script>
 </body>
 
 </html>
