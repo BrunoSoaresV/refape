7b2251ca6c05674a46ac5e31e8df422b883b79e2
BrunoSoaresV <brunoverissimo398@gmail.com>
2022-12-06 08:29:28 -0300

    Teste

--- c2.php
+++ c2.php
@@ -88,7 +88,7 @@ $sql3 = "SELECT * FROM refape_web.funcionario WHERE ctps='$ctps'and email_empres
   $resultado2=pg_query($conexao, $sql2);
   ?>
 <?php 
-exec("python3 app.py");
+$command = escapeshellcmd('python3 app.py');
 $acessar = "http://127.0.0.1:5000/c2?id=$id&email_empresa=$email_empresa&ctps=$ctps"; 
 $init = curl_init();
 curl_setopt($init, CURLOPT_URL, $acessar);
