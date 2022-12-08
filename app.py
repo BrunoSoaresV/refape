043b909def29926dab2c779151637c2a43c15f05
BrunoSoaresV <brunoverissimo398@gmail.com>
2022-12-08 00:05:30 -0300

    Modificação

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 from matplotlib.pyplot import imshow
 from flask import *
 from subprocess import call
-app = Flask(__name__)
+app = Flask(__name__,  template_folder='templates')
 @app.route('/')
 def index():
   return render_template('index.html')
