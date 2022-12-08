495b5251d6f55492602938073c76bea7c5b1564e
BrunoSoaresV <brunoverissimo398@gmail.com>
2022-12-08 19:23:52 -0300

    Teste

--- c2.php
+++ c2.php
@@ -87,9 +87,7 @@ $sql3 = "SELECT * FROM refape_web.funcionario WHERE ctps='$ctps'and email_empres
   }
   $sql2="UPDATE refape_web.funcionario SET foto='$id/$email_empresa/1.png', foto1='$id/$email_empresa/2.png' WHERE id='$id'";
   $resultado2=pg_query($conexao, $sql2);
-  $command = escapeshellcmd("app.py $id $ctps $email_empresa");
-    $resultAsString = shell_exec($command);
-    echo $resultAsString;
+  $comando=exec("python3 app.py");
 }
 pg_close($conexao);
 echo "<br/><br/>";
